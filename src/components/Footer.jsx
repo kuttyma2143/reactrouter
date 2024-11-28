7b8cf8f5ba7557136be9ba8dac991403da3fe926
kuttyma2143 <arafath5023gmail.com>
2024-11-28 22:20:47 +0530

    react

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+
+function Footer() {
+  return (
+    <>
+    <footer className="py-5 bg-dark">
+        <div className="container">
+            <p className="m-0 text-center text-white">All The Best </p>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
